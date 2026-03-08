a057259aaaef461b5d0d8a2732b7c90dfca7df61
unknown <valrus1808@gmail.com>
2026-03-08 22:52:18 +0200

    add: task added

---
+++ key_words_class.py
@@ -0,0 +1,10 @@
+class KeyWords:
+    def __init__(self):
+        self.side = "side"
+        self.topright = "topright"
+        self.bottomleft = "bottomleft"
+        self.radius = "radius"
+        self.valueerror = "Input data is invalid\n"
+        self.indexerror = "Not enough data\n"
+
+key_words = KeyWords()
