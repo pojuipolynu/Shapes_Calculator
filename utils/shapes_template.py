b62f3d9a0007cd374c8f322c7a40ae2070d61099
unknown <valrus1808@gmail.com>
2026-03-12 20:58:29 +0200

    update: rectangle class updated

--- utils/shapes_template.py
+++ utils/shapes_template.py
@@ -19,11 +19,31 @@ class Rectangle(GeometryShape):
         self.width = width
         self.length = length
 
+    @staticmethod
+    def __get_side_length(point1: list[float], point2: list[float]):
+        side_length = math.sqrt(pow(point1[0]-point2[0], 2) + pow(point1[1]-point2[1], 2))
+
+        return side_length
+
     def calculate_perimeter(self)-> float:
         return 2 * (self.width + self.length)
     
     def calculate_area(self)-> float:
         return self.width * self.length
+    
+    @classmethod
+    def parse_data(cls, user_data: list):
+        topright_index = user_data.index("topright")
+        bottomleft_index = user_data.index("bottomleft")
+
+        topright_points = [float(user_data[topright_index + 1]), float(user_data[topright_index + 2])]
+        bottomleft_points = [float(user_data[bottomleft_index + 1]), float(user_data[bottomleft_index + 2])]
+        bottomright_points = [topright_points[0], bottomleft_points[1]]
+
+        rectangle_width = Rectangle.__get_side_length(topright_points, bottomright_points)
+        rectangle_length = Rectangle.__get_side_length(bottomleft_points, bottomright_points)
+
+        return cls(rectangle_width, rectangle_length)
 
 class Square(Rectangle):
     def __init__(self, side: float):
--- utils/utils_functions.py
+++ utils/utils_functions.py
@@ -2,11 +2,6 @@ import math
 from utils.key_words_class import key_words
 from utils.shapes_template import Circle, Square, Rectangle
 
-def get_side_length(point1: list[float], point2: list[float]):
-    side_length = math.sqrt(pow(point1[0]-point2[0], 2) + pow(point1[1]-point2[1], 2))
-
-    return side_length
-
 def input_separator(user_input: str) -> dict:
     user_input_list = user_input.split()
 
@@ -38,17 +33,9 @@ def shape_clasificator(user_input: str) -> str:
             calculation_output += str(square_object) + "\n"
 
         if separated_user_input[key_words.rectangle]:
-            topright_index = separated_user_input[key_words.rectangle].index(key_words.topright)
-            bottomleft_index = separated_user_input[key_words.rectangle].index(key_words.bottomleft)
+            rectangle_object = Rectangle.parse_data(separated_user_input[key_words.rectangle])
 
-            topright_points = [float(separated_user_input[key_words.rectangle][topright_index + 1]), float(separated_user_input[key_words.rectangle][topright_index + 2])]
-            bottomleft_points = [float(separated_user_input[key_words.rectangle][bottomleft_index + 1]), float(separated_user_input[key_words.rectangle][bottomleft_index + 2])]
-            bottomright_points = [topright_points[0], bottomleft_points[1]]
-
-            rectangle_width = get_side_length(topright_points, bottomright_points)
-            rectangle_length = get_side_length(bottomleft_points, bottomright_points)
-
-            calculation_output += str(Rectangle(rectangle_width, rectangle_length)) + "\n"
+            calculation_output += str(rectangle_object) + "\n"
 
         if separated_user_input[key_words.circle]:
             radius_index = separated_user_input[key_words.circle].index(key_words.radius) + 1
